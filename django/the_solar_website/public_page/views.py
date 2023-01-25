067e11eae0906977186cb43a70df0fe93ac1be80
Alvaro-c <alvarocanasboal@gmail.com>
2023-01-25 20:56:22 +0100

    Add refresh time to the webpage

--- django/the_solar_website/public_page/views.py
+++ django/the_solar_website/public_page/views.py
@@ -5,11 +5,12 @@ def index(request):
     filename = './media/results/results.txt'
     with open(filename, 'r') as f:
         output = f.readlines()
+        meta_tag = '<meta http-equiv="refresh" content="300">'
         css = '<style>' \
               'table {border-collapse: collapse}' \
               'td { padding-right: 20px; text-align: right ; border: 1px black solid; border-collapse: collapse' \
               '}</style>'
-        result = f"{css} <table>"
+        result = "<table>"
         for line in output:
             line = line.replace('\n', '<br>')
             all_metrics = line.split(';')
@@ -25,4 +26,6 @@ def index(request):
             result = f"{result} {line}"
         result = f"{result} </table>"
 
-    return HttpResponse(result)
+    body = f"{meta_tag} {css} {result}"
+
+    return HttpResponse(body)
