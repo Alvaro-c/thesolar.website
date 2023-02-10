74d3a12302c0bedeb88bffa2c1b8aa83f9285f66
Álvaro Cañas <alvarocanasboal@gmail.com>
2023-02-10 10:15:33 +0100

    Quick fix for get_result script

--- solar_core/power/management/commands/get_result.py
+++ solar_core/power/management/commands/get_result.py
@@ -1,10 +1,10 @@
 from django.core.management import BaseCommand
 
-from power.models import Result
+from power.models import get_result
 
 
 class Command(BaseCommand):
     help = "Gets one reading result from Arduino"
 
     def handle(self, *args, **options):
-        Result.get_result()
+        get_result()
