190dcf45905f1f30f89b35a1124edcf784488d1b
Álvaro Cañas <alvarocanasboal@gmail.com>
2023-02-15 18:25:03 +0100

    Handle error while reading data from device

--- solar_core/power/views.py
+++ solar_core/power/views.py
@@ -5,10 +5,14 @@ from power.models import Result, get_result
 
 
 def index(request):
-    get_result()
-    results = Result.objects.all().order_by('-created_at')[:300]
+    error_reading = False
+    result = get_result()
+    if not result:
+        error_reading = True
+    results = Result.objects.all().order_by('-created_at')[:100]
 
     context = {
+        'error_reading': error_reading,
         'results': results,
     }
     return render(request, 'power/index.html', context)
