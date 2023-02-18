1c1b54c4c26ad91f10d1175404ce884c4c2e4e7e
Álvaro Cañas <alvarocanasboal@gmail.com>
2023-02-18 14:36:33 +0100

    SOL-4: Handle error while fetching data, deduce server load by passing process to the client

--- solar_core/power/views.py
+++ solar_core/power/views.py
@@ -5,20 +5,12 @@ from power.models import Result, get_result
 
 
 def index(request):
-    error_reading = False
-    result = get_result()
-    if not result:
-        error_reading = True
-    results = Result.objects.all().order_by('-created_at')[:100]
-
-    context = {
-        'error_reading': error_reading,
-        'results': results,
-    }
-    return render(request, 'power/index.html', context)
+    return render(request, 'power/index.html')
 
 
 def data(request):
-    results = list(Result.objects.all().order_by('-created_at')[:300].values())
+    number_rows = request.GET.get('n', '')
+    get_result()
+    results = list(Result.objects.all().order_by('-created_at')[:int(number_rows)].values())
 
     return JsonResponse(results, safe=False)
